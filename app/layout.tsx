a883c1231b8d84cdb816dc9310114cea0a877ff8
Abdullah-765 <arshadabdullah765@gmail.com>
2025-01-04 17:57:32 +0500

    hlf-completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
 import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
